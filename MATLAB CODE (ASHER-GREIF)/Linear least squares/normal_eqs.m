a0c877e21bef2fea65a41e4fecadd67263ca6b06
Charalampos Anastasiou <39501205+VARANE2003@users.noreply.github.com>
2024-10-13 00:27:52 +0300

    Update normal_eqs.m

--- MATLAB CODE (ASHER-GREIF)/Linear least squares/normal_eqs.m
+++ MATLAB CODE (ASHER-GREIF)/Linear least squares/normal_eqs.m
@@ -1,10 +1,13 @@
 clear;
 clc;
 
+% A,b given from the problem
 A = [1 0 1;2 3 5;5 3 -2;3 5 4;-1 6 3];
 b = [4 -2 5 -2 1]';
+
+% Calculating Gram matrix B = A'A and vector y=A'b
 B = A'*A;
 y = A'*b;
-x = B\y
-r = b - A*x
-A_pseudoinv = inv(B)*A'
+x = B\y                    % Solving Bx=y 
+r = b - A*x                % Calculating residual
+A_pseudoinv = inv(B)*A'    % Calculating the pseudoinverse of A
